d3a9581ae00d4a585eeb3e4869a66c721a614482
padulapankaja <padulaguruge@gmail.com>
2021-05-22 21:37:47 +0530

    add box with decoration

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,20 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.black12,
           leading: Icon(Icons.home),
         ),
-        body: Center(
-          child: Text(
-            "Hello world"
-          ),
+        body: Container(
+
+          height: 100,
+          width: 100,
+          margin: EdgeInsets.only(bottom: 5, left: 10, right: 10, top: 20),
+          alignment: Alignment.center,
+          child: Text("Container"),
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            border: Border.all(width: 4, color: Colors.grey),
+            color: Colors.blueGrey,
+            borderRadius: BorderRadius.circular(10)
         ),
+        )
       ),
     );
   }
