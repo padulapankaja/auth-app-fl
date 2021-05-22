55713c9c8fe121822bf04b52ed570fd674302168
padulapankaja <padulaguruge@gmail.com>
2021-05-22 22:50:50 +0530

    add text

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,53 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.black12,
           leading: Icon(Icons.home),
         ),
-        body: Container(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+//          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: 100,
+              margin: EdgeInsets.only(bottom: 5, left: 10, right: 10, top: 20),
+              alignment: Alignment.center,
+              child: Text("Container"),
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: Border.all(width: 4, color: Colors.grey),
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10), boxShadow: [
+                BoxShadow(color: Colors.green, spreadRadius: 3),
+              ],
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              margin: EdgeInsets.only(bottom: 5, left: 10, right: 10, top: 20),
+              alignment: Alignment.center,
+              child: Text("Container",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w800,
+        
 
-          height: 100,
-          width: 100,
-          margin: EdgeInsets.only(bottom: 5, left: 10, right: 10, top: 20),
-          alignment: Alignment.center,
-          child: Text("Container"),
-          padding: EdgeInsets.all(5),
-          decoration: BoxDecoration(
-            border: Border.all(width: 4, color: Colors.grey),
-            color: Colors.blueGrey,
-            borderRadius: BorderRadius.circular(10)
-        ),
+                ),
+              ),
+
+
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: Border.all(width: 4, color: Colors.grey),
+                color: Colors.white38,
+                borderRadius: BorderRadius.circular(10), boxShadow: [
+                BoxShadow(color: Colors.transparent, spreadRadius: 3),
+              ],
+              ),
+
+            ),
+          ],
         )
       ),
     );
