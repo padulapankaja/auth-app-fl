a69a67ea1c08c458aee42ecbdb2217787fac9fb9
padulapankaja <padulaguruge@gmail.com>
2021-05-23 13:32:03 +0530

    add image

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: "Hello Welcome",
         home: SafeArea(
             child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color.fromRGBO(230, 126, 34, 1.0),
-            elevation: 0.0,
-          ),
+//          appBar: AppBar(
+//            backgroundColor: Color.fromRGBO(230, 126, 34, 1.0),
+//            elevation: 0.0,
+
+//          ),
           body: Container(
-              color: Color.fromRGBO(230, 126, 34, 1.0),
+              decoration: BoxDecoration(
+//                  color: Color.fromRGBO(230, 126, 34, 1.0),
+//                image: new DecorationImage(image: new AssetImage("assets/images/images.jpg"),fit: BoxFit.cover)
+                  ),
               child: Column(
                 children: <Widget>[
                   Center(
                     child: Container(
-                      height: 100,
+                      height: 150,
+
 //                        width: 200,
-                      margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+                      margin: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 150),
                       decoration: BoxDecoration(
+//                        image: DecorationImage(
+////                          image: Image.ne
+//                        ),
                           color: Colors.grey,
                           borderRadius: BorderRadius.circular(12)),
                       child: Center(
                         child: Text(
-                          "Hello",
+                          "Sign In",
                           style: TextStyle(
                             fontSize: 14,
                             fontWeight: FontWeight.w600,
@@ -37,14 +46,70 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(left: 10, right: 10, top: 10),
-                    decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadius.circular(5)),
-                    child: TextField(
-                      decoration: InputDecoration(border: InputBorder.none),
-                    ),
+
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.person, color: Colors.white38),
+                                border: InputBorder.none,
+                                hintText: "johndoe@example.com",
+                                contentPadding: EdgeInsets.only(left: 0),
+                                hintStyle: TextStyle(
+                                    color: Colors.white38,
+                                    fontWeight: FontWeight.w200,
+                                    fontSize: 14)),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.lock, color: Colors.white38),
+                                border: InputBorder.none,
+                                hintText: "johndoe@example.com",
+                                contentPadding: EdgeInsets.only(left: 10),
+                                hintStyle: TextStyle(
+                                    color: Colors.white38,
+                                    fontWeight: FontWeight.w200,
+                                    fontSize: 14)),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 0),
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(color: Colors.white38),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   )
                 ],
               )),
